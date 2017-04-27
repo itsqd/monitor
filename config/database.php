d9c9abc677ec121d2f900b08b876e7e55af72c70
andreffonseca <andre.fg.fonseca@gmail.com>
2017-04-27 14:26:01 +0100

    Dev

--- config/database.php
+++ config/database.php
@@ -3,8 +3,8 @@
 return ['connections' => [
             'connection_1' => [
                 'driver' => 'mysql',
-                'username' => 'andreffonseca',
-                'password' => '0217166702',
+                'username' => 'uapi',
+                'password' => '12345',
                 'host' => '172.16.9.169',
                 'port' => '3306',
                 'database' => 'itsqd_mondb',
